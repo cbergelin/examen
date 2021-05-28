0a524c0847b746dc324753d2cdab941f85ebc32a
cbergelin <carl.bergelin@live.se>
2021-05-28 08:58:53 +0200

    header

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title: 'Hem',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Butik',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Kontakt',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Om oss',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        svg: '',
+        url: '#',
+        svgName: 'far fa-user'
+    },
+    {
+        svg: '',
+        url: '#',
+        svgName: 'fas fa-shopping-cart'
+    },
+]
